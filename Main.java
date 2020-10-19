cee56c7acd041b495f2644c4700fed0af1e205eb
Adrian Kicia <adriankicia97@gmail.com>
2020-10-19 18:33:19 +0200

    Count Unique LastNames

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ class Main {
 		PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
 
 		System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
+		System.out.println(String.format("Liczba osób z unikalnymi nazwiskami: %d", peopleStats.countUniqueLastNames()));
     }
 }
 
@@ -24,6 +25,13 @@ class PeopleStats {
 	
 	public long count() {
     return people.size();
+	}
+	
+	public long countUniqueLastNames() {
+    return people.stream()
+            .map(Person::getLastName)
+            .distinct()
+            .count();
 	}
 	
 }
